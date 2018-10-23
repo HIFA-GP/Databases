441f0ffd2a07e69abc98ff409d0e0deded418dc8
Asmer <asmer.amenar@gmail.com>
2018-10-23 16:24:18 +0200

    Inheritance done.

--- try.py
+++ try.py
@@ -12,12 +12,15 @@ class User(Base):
     __tablename__ = "user"
     id = Column('id',Integer,primary_key=True , autoincrement=True)
     username = Column('username',String , unique=True)
-    password = Column('password' , String , unique=True)
+    password = Column('password' , String)		##[Asmer] password doesn't have to be unique
     email = Column('email' , String , unique=True)
-    googleassist = Column('googleassist' , Integer)
-    alexa = Column('alexa',Integer)
+    googleassist = Column('googleassist' , String)	##[Asmer] user_id include alphabetic characters	
+    alexa = Column('alexa',String) ##[Asmer] user_id include alphabetic characters	
 ##    for one-to many relation with room table
-    room = relationship("Room")
+    room = relationship("Room",			##[Asmer] Cascade to delete associated rooms
+    	back_populates='user',			## if that user was deleted.
+        cascade='all, delete-orphan')
+
 
 class Room(Base):
     __tablename__ = "room"
@@ -26,7 +29,9 @@ class Room(Base):
 ##    for one-to many relation with room table
     user_id = Column(Integer , ForeignKey('user.id'))
 ##    for one-to many relation with device table    
-    device = relationship("Device")
+    device = relationship("Device",			##[Asmer] Cascade to delete associated devices
+    	back_populates='room',				## if that room was deleted.
+        cascade='all, delete-orphan')
 
 class Device(Base):
     __tablename__ = "device"
@@ -36,25 +41,28 @@ class Device(Base):
 ##    to specify the device table as a parent in the inheritence
     type = Column(String)
 
-    __mapper_args__ = {'polymorphic_identity':'device','polymorphic_on':type
+    __mapper_args__ = {'polymorphic_identity':'device', 'polymorphic_on': type
         }
 
 class Hub(Device):
     __tablename__ = "hub"
-    id = Column('id' , Integer , primary_key=True)
-##    inherit from the parent table device
-    device_id = Column(Integer , ForeignKey('device.id'))
+    id = Column(ForeignKey('device.id'), primary_key=True)		##[Asmer] The FK and The PK are the same here
+    ##    inherit from the parent table device
+    
+    # device_id = Column(Integer , ForeignKey('device.id'))
 
-    __mapper_args__ = {'ploymorphic_identity':'hub'
+    __mapper_args__ = {'polymorphic_identity':'hub'
         }
 
 class Controller(Device):
     __tablename__ = "controller"
-    id = Column('id' , Integer , primary_key=True)
-##    inherit from the parent table device
-    device_id = Column(Integer , ForeignKey('device.id'))
+    id = Column(ForeignKey('device.id'), primary_key=True)		##[Asmer] The FK and The PK are the same here
+    # id = Column('id' , Integer , primary_key=True)
+    ##    inherit from the parent table device
+    
+    # device_id = Column(Integer , ForeignKey('device.id'))
 
-    __mapper_args__ = {'ploymorphic_identity':'controller'
+    __mapper_args__ = {'polymorphic_identity':'controller'
         }
 
     
